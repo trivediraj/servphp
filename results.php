c309c66043c05c148dd4a4d0dd10ea70cd051b94
Rhys Streefland <rhysaled.streefland@ntlworld.com>
2011-12-06 11:05:31 +0000

    Added the session code. Re-implemented the loading icon with the templates background. Added a button so users can edit the checkbox values and re-run the test

--- results.php
+++ results.php
@@ -1,9 +1,38 @@
+<?php 
+session_start();
+include 'functions.php'; 
+    $ch1 = $_SESSION['ch1'];
+	$ch2 = $_SESSION['ch2'];
+	$ch3 = $_SESSION['ch3'];
+	$ch4 = $_SESSION['ch4'];
+	$ch5 = $_SESSION['ch5'];
+	$ch6 = $_SESSION['ch6'];
+	$ch7 = $_SESSION['ch7'];
+	$ch8 = $_SESSION['ch8'];
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="style.css" />
 <title>ServPHP</title>
+<SCRIPT TYPE="text/javascript" LANGUAGE="javascript">
+<!-- PreLoad Wait - Script -->
+<!-- This script and more from http://www.rainbow.arch.scriptmania.com 
+function waitPreloadPage() { //DOM
+if (document.getElementById){
+document.getElementById('prepage').style.visibility='hidden';
+}else{
+if (document.layers){ //NS4
+document.prepage.visibility = 'hidden';
+}
+else { //IE4
+document.all.prepage.style.visibility = 'hidden';
+}
+}
+}
+// End -->
+</SCRIPT>
 <style type="text/css">
 <!--
 body {
@@ -19,12 +48,13 @@ body {
 }
 -->
 </style>
-<?php 
-include 'functions.php'; 
-?>
 </head>
 
-<body>
+<body onLoad="waitPreloadPage();" style="margin: 0pt; padding: 0pt;">
+<DIV id="prepage" style="position:absolute; font-family:Trebuchet MS; font-size:20; background-image: url(icons/background.png); background-repeat: repeat-x;  height:100%; width:100%; z-index:1;"> 
+<div style="position:absolute; top:50%; left:50%; vertical-align:middle; text-align: center; ">
+<img src="icons/loading.gif" style="position:relative;left:-16px;top:-16px;"></div>
+</DIV>
 <div id="container">
 		<div id="header">
         	<h1>Serv<span class="off">PHP</span></h1>
@@ -44,8 +74,8 @@ include 'functions.php';
             <tr>
               <td><img src='icons/network-icon.png' alt='Load' />Server IP Address :</td>
               <td><?php
-			  if(isset($_POST['ch1']) &&
-   $_POST['ch1'] == 'selected')
+			  if(isset($ch1) &&
+               $ch1 == 'selected')
 {
     echo get_ip();
 }
@@ -59,8 +89,8 @@ else
             <tr>
               <td><img src='icons/up-alt-icon.png' alt='Uptime' />Server Uptime :</td>
               <td><?php
-			  if(isset($_POST['ch2']) &&
-   $_POST['ch2'] == 'selected')
+			  if(isset($ch2) &&
+   $ch2 == 'selected')
 {
 echo get_uptime();
 }
@@ -74,8 +104,8 @@ else
             <tr>
               <td><img src='icons/loaded-truck.png' alt='Load' />Average Load :</td>
               <td><?php
-			  if(isset($_POST['ch3']) &&
-   $_POST['ch3'] == 'selected')
+			  if(isset($ch3) &&
+   $ch3 == 'selected')
 {
 echo get_load();
 }
@@ -89,8 +119,8 @@ else
             <tr>
               <td><img src="icons/intel-2-icon.png" alt="CPU" />Server CPU Info :</td>
               <td><?php 
-			  if(isset($_POST['ch4']) &&
-   $_POST['ch4'] == 'selected')
+			  if(isset($ch4) &&
+   $ch4 == 'selected')
 {
 echo get_cpuinfo();
 }
@@ -103,8 +133,8 @@ else
             <tr>
               <td><img src="icons/Ekisho-Deep-Ocean-HD-1-icon.png" alt="HDD"/>Server Disk Usage :</td>
               <td><?php 
-			  if(isset($_POST['ch5']) &&
-   $_POST['ch5'] == 'selected')
+			  if(isset($ch5) &&
+   $ch5 == 'selected')
 {
 echo get_hdd();
 }
@@ -117,8 +147,8 @@ else
             <tr>
               <td><img src="icons/monitor-icon.png" alt="HDD" />Memory Usage :</td>
               <td><?php
-			  if(isset($_POST['ch6']) &&
-   $_POST['ch6'] == 'selected')
+			  if(isset($ch6) &&
+   $ch6 == 'selected')
 {
 echo get_memory();
 echo '%';
@@ -132,8 +162,8 @@ else
             <tr>
               <td><img src="icons/Globe-icon.png" alt="Ping"/>Your ping to this server :</td>
               <td><?php 
-			  if(isset($_POST['ch7']) &&
-   $_POST['ch7'] == 'selected')
+			  if(isset($ch7) &&
+   $ch7 == 'selected')
 {
 echo get_ping();
 }
@@ -146,8 +176,8 @@ else
             <tr>
               <td><img src="icons/down-alt-icon.png" alt="Ping"/>Server Download Speed Test :</td>
               <td><?php
-			  if(isset($_POST['ch8']) &&
-   $_POST['ch8'] == 'selected')
+			  if(isset($ch8) &&
+   $ch8 == 'selected')
 {
     echo get_speedtest(); 
 }
@@ -158,7 +188,8 @@ else
 ?></td>
             </tr>
             <tr>
-              <td colspan="2"><input name="Refresh" type=button id="Refresh" onclick="history.go()" value="Refresh" /></td>
+              <td colspan="2"><input name="Refresh" type=button id="Refresh" onclick="history.go()" value="Refresh" />
+                              <input name="Change" type=button id="change" onclick="window.location.href='index.php'" value="Change Test Options" /></td>
             </tr>
           </table>
         </div>
